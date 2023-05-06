e7eca25a7d1f29c114b4dc261af0df1fa6ccb5af
Nguyen Cuong <nguyencuonghd66@gmail.com>
2023-05-06 09:10:14 +0700

    update chatbot

---
+++ src/routes/web.js
@@ -0,0 +1,12 @@
+import express from "express";
+import HomeController from "../Controller/HomeController";
+let router = express.Router();
+
+let initWebRoutes = (app) =>{
+    router.get("/", HomeController.getHomePage);
+    router.post('/webhook', HomeController.postWebHook);
+    router.get('/webhook', HomeController.getWebHook)
+    return app.use('/',router)
+}
+
+module.exports = initWebRoutes;
