4151eb9ec731857bcd11870a83574868a4a049d6
Nguyen Cuong <nguyencuonghd66@gmail.com>
2023-05-07 13:44:27 +0700

    update

--- src/Controller/HomeController.js
+++ src/Controller/HomeController.js
@@ -109,6 +109,8 @@ function handlePostback(sender_psid, received_postback) {
         response = { "text": "Thanks!" }
     } else if (payload === 'no') {
         response = { "text": "Oops, try sending another image." }
+    } else if (payload === "GET_STARTED"){
+        response = { "text": "OK. Welcome and have a good day. Make yourselft at home!" }
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
