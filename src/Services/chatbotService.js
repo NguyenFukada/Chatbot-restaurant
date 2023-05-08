305e2d9c21c8475103316bc04e7dce99bcb40459
Nguyen Cuong <nguyencuonghd66@gmail.com>
2023-05-08 09:47:05 +0700

    update

--- src/Services/chatbotService.js
+++ src/Services/chatbotService.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import request from "request"
 require('dotenv').config();
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
@@ -22,12 +23,32 @@ function callSendAPI(sender_psid,response) {
         } else {
             console.error("Unable to send message:" + err);
         }
-    });
+    }); 
+}
+let getUserName = (sender_psid) => {
+    return new Promise((resolve, reject) => {
+        // Send the HTTP request to the Messenger Platform
+        request({
+            "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+            "method": "GET",
+        }, (err, res, body) => {
+            if (!err) {
+                body = JSON.parse(body)
+                let username = `${body.first_name} ${body.last_name}`;
+                resolve(username);
+            } else {
+                console.error("Unable to send message:" + err);
+                reject(err);
+            }
+        });
+    })
+    
 }
 let handleGetStarted = (sender_psid) => {
     return new Promise( async (resolve,reject)=>{
         try {
-            let response = { "text": "OK. Welcome and have a good day. Make yourselft at home!" }
+            let username = await getUserName(sender_psid);
+            let response = { "text": `OK. Welcome ${username} and have a good day. Make yourselft at home!` }
             await callSendAPI(sender_psid,response);
             resolve('Done');
         } catch (e) {
